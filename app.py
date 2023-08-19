6fabe61a2636610a31f16b305a18273d7b341361
jose celestino <jose8.trabalho@protonmail.com>
2023-08-19 19:10:02 -0300

    um pouco de progresso no app.py, organização dos arquivos de template

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return 'Hello World'
+    if request.method == "POST":
+        pass
+
+    return render_template('index.html')
 
 if __name__ == ("__main__"):
     app.run(debug=True)
